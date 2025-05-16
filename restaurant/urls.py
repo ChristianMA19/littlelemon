7883a90c04f0a1c4e5fe1c35cbbc250915c08a6e
Christian David Manga Arrázola <68516237+ChristianMA19@users.noreply.github.com>
2025-05-16 15:48:15 -0500

    Add Django REST framework integration with Booking and Menu APIs

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin 
 from django.urls import path 
-from .views import sayHello, index 
+from .views import sayHello, index, bookingView, menuView
   
 urlpatterns = [ 
     path('', index, name='index'),
     path('', sayHello, name='sayHello'), 
+    path('menu/',menuView.as_view(), name='menu'),
+    path('booking/', bookingView.as_view(), name='booking'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import APIView
+from rest_framework.response import Response
+from .serializers import menuSerializer, bookingSerializer
+from .models import Menu, Booking
 
 # Create your views here.
 
@@ -8,3 +12,27 @@ def sayHello(request):
 
 def index(request):
  return render(request, 'index.html', {})
+
+class bookingView(APIView):
+    def get(self, request):
+        items = Booking.objects.all()
+        serializer = bookingSerializer(items, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = bookingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data})
+        
+class menuView(APIView):
+    def get(self, request):
+        items = Menu.objects.all()
+        serializer = menuSerializer(items, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = menuSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data})
